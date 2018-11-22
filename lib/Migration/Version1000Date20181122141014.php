49f5b9de9e9095e41c9112ade74e9789cf735f2a
Joas Schilling <coding@schilljs.com>
2018-11-22 15:19:19 +0100

    Replace the too long table name by something shorter
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- lib/Migration/Version1000Date20181122141014.php
+++ lib/Migration/Version1000Date20181122141014.php
@@ -24,59 +24,22 @@ use OCP\DB\ISchemaWrapper;
 use OCP\Migration\SimpleMigrationStep;
 use OCP\Migration\IOutput;
 
-class Version1000Date20180503135440 extends SimpleMigrationStep {
+class Version1000Date20181122141014 extends SimpleMigrationStep {
 
 	/**
 	 * @param IOutput $output
 	 * @param \Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
 	 * @param array $options
 	 * @return null|ISchemaWrapper
-	 * @since 13.0.0
 	 */
 	public function changeSchema(IOutput $output, \Closure $schemaClosure, array $options) {
 		/** @var ISchemaWrapper $schema */
 		$schema = $schemaClosure();
 
-		if (!$schema->hasTable('termsofservice_terms')) {
-			$table = $schema->createTable('termsofservice_terms');
-			$table->addColumn('id', 'integer', [
-				'autoincrement' => true,
-				'notnull' => true,
-			]);
-			$table->addColumn('country_code', 'string', [
-				'notnull' => true,
-				'length' => 2,
-			]);
-			$table->addColumn('language_code', 'string', [
-				'notnull' => true,
-				'length' => 2,
-			]);
-			$table->addColumn('body', 'text', [
-				'notnull' => true,
-			]);
-			$table->setPrimaryKey(['id']);
+		if ($schema->hasTable('termsofservice_signatories')) {
+			$schema->dropTable('termsofservice_signatories');
 		}
 
-		/**
-		 * Replaced by Version1000Date20181122140802
-		if (!$schema->hasTable('termsofservice_signatories')) {
-			$table = $schema->createTable('termsofservice_signatories');
-			$table->addColumn('id', 'integer', [
-				'autoincrement' => true,
-				'notnull' => true,
-			]);
-			$table->addColumn('terms_id', 'integer', [
-				'notnull' => true,
-			]);
-			$table->addColumn('user_id', 'string', [
-				'notnull' => true,
-			]);
-			$table->addColumn('timestamp', 'integer', [
-				'notnull' => true,
-			]);
-			$table->setPrimaryKey(['id']);
-		}
-		 */
 		return $schema;
 	}
 
