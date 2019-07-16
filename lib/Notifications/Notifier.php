1b6636508c2f4ac84b155766856c54da2cbe034e
Joas Schilling <coding@schilljs.com>
2019-07-16 12:00:15 +0200

    Adjust notifier to Nextcloud 17
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- lib/Notifications/Notifier.php
+++ lib/Notifications/Notifier.php
@@ -47,13 +47,33 @@ class Notifier implements INotifier {
 	}
 
 	/**
+	 * Identifier of the notifier, only use [a-z0-9_]
+	 *
+	 * @return string
+	 * @since 17.0.0
+	 */
+	public function getID(): string {
+		return 'terms_of_service';
+	}
+
+	/**
+	 * Human readable name describing the notifier
+	 *
+	 * @return string
+	 * @since 17.0.0
+	 */
+	public function getName(): string {
+		return $this->l10nFactory->get('terms_of_service')->t('Terms of service');
+	}
+
+	/**
 	 * @param INotification $notification
 	 * @param string $languageCode The code of the language that should be used to prepare the notification
 	 * @return INotification
 	 * @throws \InvalidArgumentException When the notification was not prepared by a notifier
 	 * @since 9.0.0
 	 */
-	public function prepare(INotification $notification, $languageCode) {
+	public function prepare(INotification $notification, string $languageCode): INotification {
 		if ($notification->getApp() !== 'terms_of_service') {
 			throw new \InvalidArgumentException('Wrong app');
 		}
