7abf3549d3303d625ef8b3da1d28fb9c5c1fe8ea
Joas Schilling <coding@schilljs.com>
2020-01-29 09:15:44 +0100

    Correctly setup Application
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- appinfo/app.php
+++ appinfo/app.php
@@ -21,5 +21,5 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$app = new \OCA\TermsOfService\AppInfo\Application();
+$app = \OC::$server->query(\OCA\TermsOfService\AppInfo\Application::class);
 $app->register();
