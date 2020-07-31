000af406871468a537c5ccc40e53974673da9c5a
Joas Schilling <coding@schilljs.com>
2020-07-31 10:09:52 +0200

    Update the WebPack world
    Signed-off-by: Joas Schilling <coding@schilljs.com>

---
+++ webpack.js
@@ -0,0 +1,18 @@
+const { merge } = require('webpack-merge')
+const path = require('path')
+const webpackConfig = require('@nextcloud/webpack-vue-config')
+
+const config = {
+	entry: {
+		admin: path.join(__dirname, 'src', 'admin.js'),
+		user: path.join(__dirname, 'src', 'user.js'),
+		public: path.join(__dirname, 'src', 'public.js'),
+	},
+	output: {
+		filename: 'terms_of_service_[name].js',
+	},
+}
+
+const mergedConfigs = merge(webpackConfig, config)
+delete mergedConfigs.entry.main
+module.exports = mergedConfigs
