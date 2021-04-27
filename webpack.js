22be1aed6b86e252fbef3823c5edcf8911991498
Joas Schilling <coding@schilljs.com>
2021-04-27 10:49:57 +0200

    Move to new webpack config
    Signed-off-by: Joas Schilling <coding@schilljs.com>

--- webpack.js
+++ webpack.js
@@ -1,19 +1,11 @@
-const { merge } = require('webpack-merge')
 const path = require('path')
 const webpackConfig = require('@nextcloud/webpack-vue-config')
 
-const config = {
-	entry: {
-		admin: path.join(__dirname, 'src', 'admin.js'),
-		user: path.join(__dirname, 'src', 'user.js'),
-		public: path.join(__dirname, 'src', 'public.js'),
-		registration: path.join(__dirname, 'src', 'registration.js'),
-	},
-	output: {
-		filename: 'terms_of_service_[name].js',
-	},
-}
+delete webpackConfig.entry.main
+webpackConfig.entry.admin = path.join(__dirname, 'src', 'admin.js')
+webpackConfig.entry.user = path.join(__dirname, 'src', 'user.js')
+webpackConfig.entry.public = path.join(__dirname, 'src', 'public.js')
+webpackConfig.entry.registration = path.join(__dirname, 'src', 'registration.js')
+webpackConfig.output.filename = 'terms_of_service_[name].js'
 
-const mergedConfigs = merge(webpackConfig, config)
-delete mergedConfigs.entry.main
-module.exports = mergedConfigs
+module.exports = webpackConfig
