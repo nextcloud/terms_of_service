ceec4b26ec58c8b164413db3048adb2121c0d4d8
Andy Scherzinger <info@andy-scherzinger.de>
2024-09-09 09:44:29 +0200

    ci: Add license file generation
    Signed-off-by: Andy Scherzinger <info@andy-scherzinger.de>

--- webpack.js
+++ webpack.js
@@ -3,6 +3,7 @@
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 const path = require('path')
+const WebpackSPDXPlugin = require('./build-js/WebpackSPDXPlugin.js')
 const webpackConfig = require('@nextcloud/webpack-vue-config')
 
 webpackConfig.entry = {
@@ -11,5 +12,18 @@ webpackConfig.entry = {
 	public: path.join(__dirname, 'src', 'public.js'),
 	registration: path.join(__dirname, 'src', 'registration.js'),
 }
+
+webpackConfig.plugins = [
+	...webpackConfig.plugins,
+	// Generate reuse license files
+	new WebpackSPDXPlugin({
+		override: {
+			// TODO: Remove if they fixed the license in the package.json
+			'@nextcloud/axios': 'GPL-3.0-or-later',
+			'@nextcloud/vue': 'AGPL-3.0-or-later',
+			'nextcloud-vue-collections': 'AGPL-3.0-or-later',
+		}
+	}),
+]
 
 module.exports = webpackConfig
