4e22755174ff0be03c0f4929b67f2d92eb0b5d0c
Andy Scherzinger <info@andy-scherzinger.de>
2024-09-09 11:00:24 +0200

    fix: don't create LICENSE.TXT anymore
    Signed-off-by: Andy Scherzinger <info@andy-scherzinger.de>

--- webpack.js
+++ webpack.js
@@ -6,6 +6,8 @@ const path = require('path')
 const WebpackSPDXPlugin = require('./build-js/WebpackSPDXPlugin.js')
 const webpackConfig = require('@nextcloud/webpack-vue-config')
 
+const isDev = process.env.NODE_ENV === 'development'
+
 webpackConfig.entry = {
 	admin: path.join(__dirname, 'src', 'admin.js'),
 	user: path.join(__dirname, 'src', 'user.js'),
@@ -13,17 +15,34 @@ webpackConfig.entry = {
 	registration: path.join(__dirname, 'src', 'registration.js'),
 }
 
-webpackConfig.plugins = [
-	...webpackConfig.plugins,
-	// Generate reuse license files
-	new WebpackSPDXPlugin({
+// Generate reuse license files if not in development mode
+if (!isDev) {
+	webpackConfig.plugins.push(new WebpackSPDXPlugin({
 		override: {
-			// TODO: Remove if they fixed the license in the package.json
+			select2: 'MIT',
 			'@nextcloud/axios': 'GPL-3.0-or-later',
 			'@nextcloud/vue': 'AGPL-3.0-or-later',
 			'nextcloud-vue-collections': 'AGPL-3.0-or-later',
-		}
-	}),
-]
+		},
+	}))
+
+	webpackConfig.optimization.minimizer = [{
+		apply: (compiler) => {
+			// Lazy load the Terser plugin
+			const TerserPlugin = require('terser-webpack-plugin')
+			new TerserPlugin({
+				extractComments: false,
+				terserOptions: {
+					format: {
+						comments: false,
+					},
+					compress: {
+						passes: 2,
+					},
+				},
+		  }).apply(compiler)
+		},
+	}]
+}
 
 module.exports = webpackConfig
